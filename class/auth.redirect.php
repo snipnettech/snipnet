408fba2ab4f17c13f1d8cc1fa8fe8c59effa6130
Choice Douglas <46899181+snipnettech@users.noreply.github.com>
2019-01-22 11:22:15 +0100

    Revert "default"
    This reverts commit 29ef3fdd8296ba4ea0b8eeab0ede5c993a341e7d.

--- class/auth.redirect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: index");
-    exit;
-}
-?>
